c1543b67719a6625fc4dde871ea9fccee0933e05
Manuel <nicocohen56@gmail.com>
2026-06-09 17:35:58 +0100

    first commit

---
+++ api/index.ts
@@ -0,0 +1,5 @@
+
+// api/index.ts
+import express from 'express';
+// Aqui importamos as rotas e regras do seu server.ts adaptadas para exportar o 'app' sem dar `.listen()` se executado sobre ambiente serverless.
+
